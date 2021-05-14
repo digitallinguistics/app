0560b46d3596d37ded72c84657f30acd67d128da
Daniel W. Hieber <dwhieb@gmail.com>
2021-05-14 01:48:40 -0500

    DEV: add local server for development

---
+++ server.js
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'url';
+import handler           from 'serve-handler';
+import http              from 'http';
+
+import {
+  dirname as getDirname,
+  join    as joinPath,
+} from 'path';
+
+const currentDir = getDirname(fileURLToPath(import.meta.url));
+const distDir    = joinPath(currentDir, './dist');
+
+const port   = 3000 || process.env.PORT;
+const server = http.createServer((req, res) => handler(req, res, { public: distDir }));
+
+server.listen(port, () => console.info(`Server started on port ${port}. Press Ctrl+C to terminate.`));
+server.on('error', console.error);
