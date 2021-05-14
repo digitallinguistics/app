be60d3e334a246342250a8341c5853f08a99cf96
Daniel W. Hieber <dwhieb@gmail.com>
2021-05-14 00:01:47 -0500

    DEV: add build script (#26)
    closes #26

---
+++ build/index.js
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url';
+
+import {
+  copy,
+  emptyDir,
+} from 'fs-extra';
+
+import {
+  dirname as getDirname,
+  join    as joinPath,
+} from 'path';
+
+const currentDir = getDirname(fileURLToPath(import.meta.url));
+const srcDir     = joinPath(currentDir, '../src')
+const distDir    = joinPath(currentDir, '../dist')
+
+await emptyDir(distDir);
+await copy(joinPath(srcDir, 'index.html'), joinPath(distDir, 'index.html'));
