311d6251ab7bf647f190166aa7b2d491098a5ffb
Daniel W. Hieber <dwhieb@gmail.com>
2021-05-14 01:28:29 -0500

    DEV: add app manifest + icons (#21, #28, #29)
    closes #21
    closes #28
    closes #29

--- build/index.js
+++ build/index.js
@@ -15,4 +15,7 @@ const srcDir     = joinPath(currentDir, '../src');
 const distDir    = joinPath(currentDir, '../dist');
 
 await emptyDir(distDir);
+await copy(joinPath(srcDir, '/images'), joinPath(distDir, '/images'));
+await copy(joinPath(srcDir, 'favicon.ico'), joinPath(distDir, 'favicon.ico'));
 await copy(joinPath(srcDir, 'index.html'), joinPath(distDir, 'index.html'));
+await copy(joinPath(srcDir, 'manifest.json'), joinPath(distDir, 'manifest.json'));
