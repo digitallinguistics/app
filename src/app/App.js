c3eb41e67895fa6ab56d59bb62071de79f28bd3b
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-06-25 11:52:12 -0500

    Create App.js

---
+++ src/app/App.js
@@ -0,0 +1,22 @@
+/**
+ * @namespace app
+ */
+
+/**
+ * The top-level view for the app
+ * @memberof app
+ * @instance
+ */
+export default class App {
+
+  // PROPERTIES
+
+  // METHODS
+
+  /**
+   * Initialize the App view
+   * @return {Promise}
+   */
+  async initialize() {}
+
+  }
