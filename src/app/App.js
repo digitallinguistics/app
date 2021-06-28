9aac2656c4a1cc4c2bf4f840e5e157bac8698a45
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-06-28 12:09:57 -0500

    Create App.announce() function

--- src/app/App.js
+++ src/app/App.js
@@ -7,4 +7,26 @@
  * @memberof app
  * @instance
  */
-export default class App {}
+export default class App {
+
+  // PROPERTIES
+  /**
+   * References to DOM elements used by the App View
+   * @type {Object}
+   * @prop {HTMLElement} info   - A reference to the ARIA live region (`<p id=info hidden aria-live=polite>`) where content is announced to screen readers
+   */
+  nodes = {
+    info: document.getElementById(`info`),
+  }
+
+  // METHODS
+
+  /**
+   * Updates the text of the ARIA live region so that the text will be announced to screen readers
+   * @param {String} text The text to announce
+   */
+  announce(text) {
+    this.nodes.info.textContent = text;
+  }
+
+}
