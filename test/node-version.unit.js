25052b52cf1b02314399b568aab4faeaecda727c
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-06-29 21:17:36 -0500

    create node version test

---
+++ test/node-version.unit.js
@@ -0,0 +1,26 @@
+import { expect }        from 'chai';
+import { fileURLToPath } from 'url';
+import fs                from 'fs-extra';
+import semver            from 'semver';
+
+import {
+  dirname as getDirname,
+  join    as joinPath,
+} from 'path';
+
+const { readFile } = fs;
+const currentDir   = getDirname(fileURLToPath(import.meta.url));
+const packageJSON  = JSON.parse( await readFile(joinPath(currentDir, '../package.json'), 'utf8'));
+const packageVer   = packageJSON.engines.node;
+
+describe('active Node version', function() {
+
+  it('matches Node version in package.json', function() {
+
+    const localVer = process.version;
+
+    expect(semver.major(semver.minVersion(packageVer))).to.equal(semver.major(localVer));
+
+  });
+
+});
