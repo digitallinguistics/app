a842003da6f9d41c4b4186926d65792473448587
Daniel W. Hieber <dwhieb@gmail.com>
2021-08-09 23:37:03 -0600

    finish LanguagesList

--- build/bin/startServer.js
+++ build/bin/startServer.js
@@ -1,3 +1,3 @@
-import startServer from '../../server.cjs';
+import startServer from '../../server.js';
 
 startServer();
--- src/pages/Languages/LanguagesList/LanguagesList.js
+++ src/pages/Languages/LanguagesList/LanguagesList.js
@@ -10,22 +10,16 @@ import View     from '../../../core/View.js';
 export default class LanguagesList extends View {
 
   /**
+   * The languages included in this view.
+   * @type {Array<models#Language>}
+   */
+  languages = [];
+
+  /**
    * A reference to the `<template>` tag for this view.
    * @type {HTMLTemplateElement}
    */
   template = document.getElementById(`languages-list-template`);
-
-  /**
-   * The templating function for each language in the list.
-   * @param  {Object} language The Language object to render a `<li>` element for.
-   * @return {HTMLLIElement}
-   */
-  static itemTemplate({ cid, name }) {
-    const li            = document.createElement(`li`);
-    li.dataset.language = cid;
-    [li.textContent]    = Object.values(name);
-    return li;
-  }
 
   /**
    * Create a new Languages List
@@ -38,9 +32,9 @@ export default class LanguagesList extends View {
 
   /**
    * Render the Languages List
-   * @return {HTMLNavElement}
+   * @return {HTMLElement}
    */
-  render() {
+  render(languageCID) {
 
     this.el        = this.cloneTemplate();
     const listView = new ListView(this.languages, { template: LanguagesList.itemTemplate });
@@ -49,11 +43,52 @@ export default class LanguagesList extends View {
 
     oldList.replaceWith(this.list);
 
-    // TODO: add listeners
-    // TODO: set current language
+    this.addEventListeners();
+
+    if (languageCID) this.setCurrentLanguage(languageCID);
 
     return this.el;
 
+  }
+
+  /**
+   * Set the currently selected language in the list
+   * @param {String} languageCID The client ID of the language to set as selected
+   */
+  setCurrentLanguage(languageCID) {
+
+    const items = Array.from(this.list.children);
+
+    if (items.length) {
+
+      // clear the currently selected language from all items
+
+      for (const item of items) {
+        item.removeAttribute(`aria-current`);
+        item.classList.remove(`current`);
+      }
+
+      // set the currently selected language
+
+      const currentItem = items.find(item => item.dataset.language === languageCID);
+
+      currentItem.setAttribute(`aria-current`, `language`);
+      currentItem.classList.add(`current`);
+
+    }
+
+  }
+
+  /**
+   * The templating function for each language in the list.
+   * @param  {Object} language The Language object to render a `<li>` element for.
+   * @return {HTMLLIElement}
+   */
+  static itemTemplate({ cid, name }) {
+    const li            = document.createElement(`li`);
+    li.dataset.language = cid;
+    [li.textContent]    = Object.values(name);
+    return li;
   }
 
 }
