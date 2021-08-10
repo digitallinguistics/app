51e50085a85acded3569cb66e03179bc5aa11a17
Daniel W. Hieber <dwhieb@gmail.com>
2021-08-09 21:38:38 -0600

    more component testing setup

--- test/plugins/index.cjs
+++ test/plugins/index.cjs
@@ -2,9 +2,9 @@ const startServer = require(`../../server.cjs`);
 
 module.exports = function plugins(on, config) {
 
-  // if (config.testingType === `component`) {
+  if (config.testingType === `component`) {
     on(`dev-server:start`, () => startServer());
-  // }
+  }
 
   return config;
 
