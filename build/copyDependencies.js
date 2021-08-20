baf9c752d5974f7f2243e13e2e6d736eca9fcedc
Daniel W. Hieber <dwhieb@gmail.com>
2021-08-19 18:20:05 -0600

    add Model tests

--- build/copyDependencies.js
+++ build/copyDependencies.js
@@ -11,6 +11,6 @@ const currentDir = getDirname(fileURLToPath(import.meta.url));
 export default async function copyAssets() {
   await copy(
     joinPath(currentDir, `../node_modules/uuid/dist/esm-browser`),
-    joinPath(currentDir, `../vendor/UUID`),
+    joinPath(currentDir, `../src/vendor/UUID`),
   );
 }
