5b8e7c1f54eec3c1952d3a448c52a7eb0566e5c0
Daniel W. Hieber <dwhieb@gmail.com>
2021-08-31 22:09:55 -0600

    Language Editor > Add a Language button

--- src/pages/Languages/Languages.test.js
+++ src/pages/Languages/Languages.test.js
@@ -1,18 +1,17 @@
 describe(`Languages`, () => {
 
   before(function() {
-    cy.fixture(`languages.json`).as(`languages`);
-  });
-
-  it(`renders`, () => {
-
     cy.visit(`/`);
-
     cy.get(`#nav li[data-page=Languages]`)
     .click();
+  });
 
-    cy.get(`#main[data-page=Languages] .languages-list`);
+  it(`renders the Add a Language button when there are no languages`, function() {
+    cy.get(`#main[data-page=Languages] .language-editor .add-language-button`);
+  });
 
+  it(`adds a language`, function() {
+    cy.get(`#main[data-page=Languages] .languages-list .add-language-button`);
   });
 
 });
