bc841d4bd0008578c66e63f21cf11b3cf625e3a7
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-04 21:48:54 -0600

    Languages tests

--- src/pages/Languages/Languages.test.js
+++ src/pages/Languages/Languages.test.js
@@ -1,16 +1,15 @@
 describe(`Languages`, () => {
 
-  before(function() {
+  it(`adds a language from the Language Editor`, function() {
     cy.visit(`/`);
     cy.get(`#nav[data-loaded=true] li[data-page=Languages]`).click();
-  });
-
-  it.only(`adds a language from the Language Editor`, function() {
     cy.get(`.language-editor .add-language-button`).click();
     cy.contains(`.languages-list`, `{ new language }`);
   });
 
   it(`adds a language from the Languages List`, function() {
+    cy.visit(`/`);
+    cy.get(`#nav[data-loaded=true] li[data-page=Languages]`).click();
     cy.get(`.languages-list .add-language-button`).click();
     cy.contains(`.languages-list`, `{ new language }`);
     // TODO: Check that Language Editor renders with an empty form.
