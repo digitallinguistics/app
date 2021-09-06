1af7ca72c476b67c81567b45f138afe35b7599d3
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-06 01:31:53 -0600

    New: Button (#152)
    closes #152

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,31 @@
+import './Button.css';
+import html2element from '../../utilities/html2element.js';
+
+const html = `<div class=buttons>
+
+  <style>
+  
+    .buttons {
+      align-items: flex-start;
+    }
+    
+    .button {
+      margin: 1em;
+      width:  5em;
+    }
+  
+  </style>
+  
+  <button class='button'>Default</button>
+  <button class='button blue'>Primary</button>
+  <button class='button green'>Success</button>
+  <button class='button red'>Alert</button>
+
+</div>`;
+
+export default {
+  layout: `centered`,
+  title:  `Button`,
+};
+
+export const Button = () => html2element(html);
