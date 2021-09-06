906321b498adbd149275fe1fc92d962517a16ca6
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-06 15:08:40 -0600

    tests passing

--- src/pages/Languages/Languages.test.js
+++ src/pages/Languages/Languages.test.js
@@ -1,13 +1,21 @@
 describe(`Languages`, () => {
 
-  it(`adds a language from the Language Editor`, function() {
+  it(`adds / deletes a language from the Language Editor`, function() {
     cy.visit(`/`);
     cy.get(`#nav[data-loaded=true] li[data-page=Languages]`).click();
     cy.get(`.language-editor .add-language-button`).click();
     cy.contains(`.languages-list`, `{ new language }`);
+    cy.window().then(win => {
+      cy.stub(win, `prompt`).returns(`YES`);
+      cy.contains(`.language-editor`, `Delete this Language`).click();
+      cy.get(`.languages-list .languages`).children()
+      .should(`have.length`, 0);
+      // should render a blank Language Editor
+      cy.get(`.language-editor .add-language-button`);
+    });
   });
 
-  it(`adds a language from the Languages List`, function() {
+  it(`adds / edits a language from the Languages List`, function() {
     cy.visit(`/`);
     cy.get(`#nav[data-loaded=true] li[data-page=Languages]`).click();
     cy.get(`.languages-list .add-language-button`).click();
