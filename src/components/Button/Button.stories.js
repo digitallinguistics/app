8fccc72021b2cf4aeece6276022af311f6cc1e67
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-06 00:45:40 -0600

    add button styles

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,8 +1,31 @@
 import './Button.css';
 import html2element from '../../utilities/html2element.js';
 
+const html = `<div class=buttons>
+
+  <style>
+  
+    .buttons {
+      align-items: flex-start;
+    }
+    
+    .button {
+      margin: 1em;
+      width:  5em;
+    }
+  
+  </style>
+  
+  <button class='button'>Default</button>
+  <button class='button primary'>Primary</button>
+  <button class='button success'>Success</button>
+  <button class='button alert'>Alert</button>
+
+</div>`;
+
 export default {
-  title: `Button`,
+  layout: `centered`,
+  title:  `Button`,
 };
 
-export const Button = () => html2element(`<button class='button'>Click me!</button>`);
+export const Button = () => html2element(html);
