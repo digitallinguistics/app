fe2ab12f7160f4a00b35f9bf8d09177c68994476
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-18 18:17:37 -0600

    HTML for edit button

--- src/pages/Languages/LanguageEditor/LanguageEditor.js
+++ src/pages/Languages/LanguageEditor/LanguageEditor.js
@@ -202,9 +202,11 @@ export default class LanguageEditor extends View {
         <span class=txn>${ name }</span>
         (${ language })
       </p>
-        <button class='button transparent' type=button>
-          <svg><use xlink:href=#trash></use></svg>
-          <span class=visually-hidden>Delete this Language Name</span>
+        <button aria-label='Edit this Language Name' class='button transparent' type=button>
+          <svg><use href=#edit-line></use></svg>
+        </button>
+        <button aria-label='Delete this Language Name' class='button transparent' type=button>
+          <svg><use href=#trash ></use></svg>
         </button>
     </li>`);
   }
