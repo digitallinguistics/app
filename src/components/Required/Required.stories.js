25caa688798875b7751a736dde93e73442da6cbd
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-09-20 16:44:18 -0500

    create .required class

---
+++ src/components/Required/Required.stories.js
@@ -0,0 +1,7 @@
+import './Required.css';
+
+export default {
+  title: `Components/Required`,
+};
+
+export const Required = () => `<legend class=legend><required>Required legend</required></legend>`;
