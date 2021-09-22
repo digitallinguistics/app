30a1bcd99475fb4a51d2e8386357120b5b8f2236
vadekh <36901345+vadekh@users.noreply.github.com>
2021-09-22 14:00:43 -0500

    ACCESS: test ARIA live region (#90)
    closes #90

---
+++ src/App/App.test.js
@@ -0,0 +1,11 @@
+describe(`App`, function() {
+  it(`updates ARIA live region on page change`, function() {
+      cy.visit(`/`);
+
+      cy.get(`#info`).should(`contain`, `Home page`);
+
+      cy.contains(`#nav li`,`Languages`).click();
+
+      cy.get(`#info`).should(`contain`, `Languages page`);
+  });
+});
