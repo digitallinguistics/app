87ef17e2ee889cf08c4995fa520f278b463ab58f
Tong Jiao <71759137+TongJ05@users.noreply.github.com>
2021-09-24 16:15:29 -0500

    COMPONENT: add Abbreviation story (#199)
    closes #199

---
+++ src/components/Abbreviation/Abbreviation.stories.js
@@ -0,0 +1,8 @@
+import './Abbreviation.css';
+
+export default {
+  title: `Components/Abbreviation`,
+};
+
+export const Required = () => `<p>The acronym <abbr class="abbr" title="Digital Linguistics">DLx</abbr> stands for "Digital Linguistics".</p>
+`;
