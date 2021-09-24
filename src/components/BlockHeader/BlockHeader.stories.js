195561b202b204bec03dbe02b03ac6bacd98863e
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-24 00:57:12 -0600

    Home page story

---
+++ src/components/BlockHeader/BlockHeader.stories.js
@@ -0,0 +1,7 @@
+import './BlockHeader.css';
+
+export default {
+  title: `Components/Block Header`,
+};
+
+export const BlockHeader = () => `<h1 class=block-header>Block Header</h1>`;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,7 +16,15 @@ export default class HomePage extends View {
     this.template = document.getElementById(`home-template`);
     this.el       = this.cloneTemplate();
     this.el.view  = this;
+    if (app.installEvent) this.showInstallPrompt();
     return this.el;
+  }
+
+  showInstallPrompt() {
+    this.el.querySelector(`#install-prompt`).hidden = false;
+    this.el.querySelector(`#install-prompt button`).addEventListener(`click`, () => {
+      app.installEvent.prompt();
+    });
   }
 
 }
