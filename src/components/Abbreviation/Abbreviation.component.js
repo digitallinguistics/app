7c9b8f88e68599ae857992655585e646794237f4
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-26 14:43:41 -0600

    update test types for specific tests

--- src/components/Abbreviation/Abbreviation.component.js
+++
@@ -1,7 +0,0 @@
-describe(`Cypress test`, function() {
-  it(`works`, function() {
-    cy.visit(`http://localhost:6006/iframe.html?id=components-abbreviation--required`);
-    cy.get(`abbr`)
-    .should(`have.text`, `DLx`);
-  });
-});
