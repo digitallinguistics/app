804966872348075a9c271d4e4baae0ef36172000
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-26 12:53:49 -0600

    update test scripts

---
+++ test/plugins/index.cjs
@@ -0,0 +1,7 @@
+const startServer = require(`../../server.cjs`);
+
+module.exports = (on, config) => {
+  if (config.testingType === `component`) {
+    on(`dev-server:start`, startServer);
+  }
+};
