a907a5f3019f30c409b50ce5fd58ca6ac8dee4ef
Daniel W. Hieber <dwhieb@gmail.com>
2021-09-26 13:08:07 -0600

    manual component tests working

--- src/components/Abbreviation/Abbreviation.component.js
+++ src/components/Abbreviation/Abbreviation.component.js
@@ -1,6 +1,6 @@
 describe(`Cypress test`, function() {
   it(`works`, function() {
-    cy.visit(`http://localhost:3000/iframe.html?id=components-abbreviation--required`);
+    cy.visit(`http://localhost:6006/iframe.html?id=components-abbreviation--required`);
     cy.get(`abbr`)
     .should(`have.text`, `DLx`);
   });
--- test/plugins/index.cjs
+++
@@ -1,7 +0,0 @@
-const startServer = require(`../../server.cjs`);
-
-module.exports = (on, config) => {
-  if (config.testingType === `component`) {
-    on(`dev-server:start`, startServer);
-  }
-};
