887563070c5c737c82c4fcdec6f19744670d5909
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-09-30 13:58:58 -0500

    change focus on action

--- src/pages/Languages/Languages.js
+++ src/pages/Languages/Languages.js
@@ -29,6 +29,7 @@ export default class LanguagesPage extends View {
     app.settings.language = language.cid;
     this.renderNav(language.cid);
     this.renderEditor(language.cid);
+    document.querySelector(`.language-editor input`).focus();
   }
 
   async deleteLanguage(languageCID) {
@@ -40,6 +41,7 @@ export default class LanguagesPage extends View {
     this.languages.splice(i, 1);
     this.renderNav();
     this.renderEditor();
+    document.querySelector(`.languages-nav a`).focus();
   }
 
   /**
