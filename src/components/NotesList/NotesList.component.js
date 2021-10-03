68844a6d120ce19fe905eb4906530b80ac031294
Daniel W. Hieber <dwhieb@gmail.com>
2021-10-03 16:54:21 -0600

    component tests for Notes List passing

--- src/components/NotesList/NotesList.component.js
+++ src/components/NotesList/NotesList.component.js
@@ -11,24 +11,24 @@ describe(`Notes List`, function() {
     
     cy.contains(`h3`, `Notes (0)`);
     
-    cy.get(`.js-add-note-button`)
+    cy.get(`.js-notes-list__add-note-button`)
     .click();
     
     cy.contains(`h3`, `Notes (1)`);
     
-    cy.get(`.notes`)
+    cy.get(`.js-notes-list__notes`)
     .children()
     .should(`have.lengthOf`, 1);
     
     cy.focused()
-    .should(`have.class`, `js-text-input`);
+    .should(`have.class`, `js-note__text-input`);
     
-    cy.get(`.note .js-delete-button`)
+    cy.get(`.note .js-note__delete-button`)
     .click();
     
     cy.contains(`h3`, `Notes (0)`);
 
-    cy.get(`.notes`)
+    cy.get(`.js-notes-list__notes`)
     .children()
     .should(`have.lengthOf`, 0);
 
@@ -38,26 +38,26 @@ describe(`Notes List`, function() {
 
     cy.visit(`http://localhost:6006/iframe.html?id=components-notes-list--collapsed`);
 
-    cy.contains(`.notes .note-item:first-child .js-text-preview`, `Note A`)
+    cy.contains(`.js-notes-list__notes .js-notes-list__note-item:first-child .js-note__text-preview`, `Note A`)
     .should(`not.be.visible`);
 
     // click a note
 
-    cy.get(`.notes .note-item:first-child .js-note-button`)
+    cy.get(`.js-notes-list__notes .js-notes-list__note-item:first-child .js-notes-list__note-button`)
     .click();
 
-    cy.contains(`.notes .note-item:first-child .js-text-preview`, `Note A`)
+    cy.contains(`.js-notes-list__notes .js-notes-list__note-item:first-child .js-note__text-preview`, `Note A`)
     .should(`be.visible`);
 
     // add a note
 
-    cy.get(`.js-add-note-button`)
+    cy.get(`.js-notes-list__add-note-button`)
     .click();
 
     cy.focused()
-    .should(`have.class`, `js-text-input`);
+    .should(`have.class`, `js-note__text-input`);
 
-    cy.contains(`.notes .note-item:nth-child(2) .js-text-preview`, `Note A`)
+    cy.contains(`.js-notes-list__notes .js-notes-list__note-item:nth-child(2) .js-note__text-preview`, `Note A`)
     .should(`not.be.visible`);
 
     // expand
@@ -65,7 +65,7 @@ describe(`Notes List`, function() {
     cy.get(`h3`)
     .click();
 
-    cy.contains(`.notes .note-item:nth-child(2) .js-text-preview`, `Note A`)
+    cy.contains(`.js-notes-list__notes .js-notes-list__note-item:nth-child(2) .js-note__text-preview`, `Note A`)
     .should(`be.visible`);
 
   });
