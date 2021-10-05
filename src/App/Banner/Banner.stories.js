17003d3f3717a459fd06110f4fc1eb1081bb060b
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-10-04 19:46:24 -0500

    add simple stories

---
+++ src/App/Banner/Banner.stories.js
@@ -0,0 +1,9 @@
+import './Banner.css';
+
+export default {
+  title: `App/Banner`,
+};
+
+export const Banner = () => `<header id=banner class=banner role=banner>
+  <svg aria-hidden=true><use href=#lotus.white /></svg>
+</header>`;
