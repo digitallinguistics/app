d83b72fffa1aa4eff6124931ebd0358ad553d8a1
Daniel W. Hieber <dwhieb@gmail.com>
2021-10-05 22:27:27 -0600

    imitate banner in story

--- src/App/HelpMenu/HelpMenu.stories.js
+++ src/App/HelpMenu/HelpMenu.stories.js
@@ -1,10 +1,30 @@
 import './HelpMenu.css';
+import html2element from '../../utilities/html2element.js';
 
 export default {
+  parameters: {
+    layout: `fullscreen`,
+  },
   title: `App/Help Menu`,
 };
 
 export const HelpMenu = () => {
+  
+  const banner = html2element(`<header id=help-menu-banner>
+    <style>
+      #help-menu-banner {
+        background-color: var(--blue);
+        flex-direction  : row;
+        justify-content : flex-end;
+        width           : 100%;
+      }
+    </style>
+  </header>`);
+
   const template = document.getElementById(`help-menu-template`);
-  return template.content.cloneNode(true).firstElementChild;
+  const menu     = template.content.cloneNode(true).firstElementChild;
+  
+  banner.appendChild(menu);
+  return banner;
+
 };
