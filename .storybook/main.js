8cadcff8afdf0008b209b292e8b7502fc5a6e97d
Daniel W. Hieber <dwhieb@gmail.com>
2021-10-12 20:29:06 -0600

    fix .browserslistrc problem

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,12 @@ module.exports = {
     `@storybook/addon-links`,
     `@storybook/addon-essentials`,
   ],
+  async babel(options) {
+    // This prevents Storybook from using this projects .browserslistrc file,
+    // which breaks the Storybook build for some reason.
+    options.targets = `defaults`;
+    return options;
+  },
   stories: [
     `../src/**/*.stories.mdx`,
     `../src/**/*.stories.@(js|jsx|ts|tsx)`,
