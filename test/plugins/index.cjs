a690c8f6ed98905d5ca3f4693fa77cebae824889
Daniel W. Hieber <dwhieb@gmail.com>
2021-10-14 20:21:13 -0600

    preprocess E2E tests with ESBuild

--- test/plugins/index.cjs
+++ test/plugins/index.cjs
@@ -1,6 +1,18 @@
-const webpackPreprocessor = require(`@cypress/webpack-preprocessor`);
-const webpackOptions      = require(`../../build/webpack.config.cjs`);
+const { build }         = require(`esbuild`);
+const esbuildPluginLESS = require(`../../build/esbuildPluginLESS.cjs`);
 
 module.exports = (on, config) => {
-  on(`file:preprocessor`, webpackPreprocessor({ webpackOptions }));
+  on(`file:preprocessor`, async ({ filePath, outputPath, shouldWatch }) => {
+
+    await build({
+      bundle:      true,
+      entryPoints: [filePath],
+      format:      `cjs`,
+      outfile:     outputPath,
+      plugins:     [esbuildPluginLESS],
+    });
+
+    return outputPath;
+
+  });
 };
--- test/support/commands.cjs
+++ test/support/commands.cjs
@@ -1,4 +1,4 @@
-import setupStore from './setupStore.js';
+const setupStore = require(`./setupStore.cjs`);
 
 Cypress.Commands.add(`setupStore`, storeName => cy.fixture(`/${ storeName }.json`)
 .then(items => setupStore(storeName, items)));
