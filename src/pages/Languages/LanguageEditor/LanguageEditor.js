6f5427c90a6c5fc7375571fccdac801543b77c19
Tong Jiao <71759137+TongJ05@users.noreply.github.com>
2021-10-15 15:16:29 -0500

    Add autocapitalize attribute to relevant fields

--- src/pages/Languages/LanguageEditor/LanguageEditor.js
+++ src/pages/Languages/LanguageEditor/LanguageEditor.js
@@ -132,9 +132,9 @@ export default class LanguageEditor extends View {
     const autonymField = this.el.querySelector(`.js-language-editor__autonym`);
 
     const txnEditor = new TranscriptionEditor(this.language.autonym, {
-      lang:        this.language.iso,
-      placeholder: `e.g. español`,
-      prefix:      `autonym`,
+      inputAttributes: { autocapitalize: `words`, placeholder: `e.g. español` },
+      lang:            this.language.iso,
+      prefix:          `autonym`,
     });
 
     autonymField.appendChild(txnEditor.render());
@@ -153,7 +153,7 @@ export default class LanguageEditor extends View {
 
     this.el.querySelector(`.js-language-editor__date-created`).textContent  = dateCreated;
     this.el.querySelector(`.js-language-editor__date-modified`).textContent = dateModified;
-    
+
   }
 
   renderName() {
@@ -161,9 +161,10 @@ export default class LanguageEditor extends View {
     const nameField = this.el.querySelector(`.js-language-editor__name`);
 
     const mlsEditor = new MultiLangStringEditor(this.language.name, {
-      fieldName:   `name`,
-      id:          `name`,
-      placeholder: `e.g. Spanish`,
+      fieldName:       `name`,
+      id:              `name`,
+      inputAttributes: { autocapitalize: `words`, placeholder: `e.g. Spanish` },
+
     });
 
     nameField.appendChild(mlsEditor.render());
