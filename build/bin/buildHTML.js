3d5f147203e0ea484d72038aa86eddd88ef6cc75
Daniel W. Hieber <dwhieb@gmail.com>
2021-10-15 22:56:08 -0600

    clean up build code

--- build/bin/buildHTML.js
+++
@@ -1,4 +0,0 @@
-import buildHTML from '../buildHTML.js';
-import ora       from 'ora';
-
-ora.promise(buildHTML().catch(console.error), `Build HTML`);
--- build/buildAppShell.js
+++ build/buildAppShell.js
@@ -4,18 +4,14 @@ import createSprites     from './createSprites.js';
 import { fileURLToPath } from 'url';
 import fs                from 'fs-extra';
 import hbs               from 'handlebars';
-import { paramCase }     from 'param-case';
-import recurse           from 'readdirp';
 import registerPartials  from './registerPartials.js';
 
 const {
-  readdir: readDir,
   readFile,
   outputFile: writeFile,
 } = fs;
 
 import {
-  basename as getBasename,
   dirname  as getDirname,
   join     as joinPath,
 } from 'path';
@@ -23,31 +19,6 @@ import {
 const currentDir = getDirname(fileURLToPath(import.meta.url));
 const srcDir     = joinPath(currentDir, `../src`);
 const distDir    = joinPath(currentDir, `../dist`);
-const pagesDir   = joinPath(srcDir, `pages`);
-
-/**
- * Builds the HTML for a single page, given an file entry returned [readdirp](https://www.npmjs.com/package/readdirp). The CSS is inserted in a `<style>` tag just inside the opening `<main>` tag.
- * @param {String} page
- */
-async function buildPageContent(page) {
-
-  const templates = await recurse(joinPath(pagesDir, page), { fileFilter: `*.hbs` });
-
-  let html = ``;
-
-  for await (const entry of templates) {
-
-    const templateHTML  = await buildTemplate(hbs, entry.fullPath);
-    const componentName = getBasename(entry.basename, `.hbs`);
-    const templateID    = `${ paramCase(componentName) }-template`;
-
-    html += `<template id=${ templateID }>${ templateHTML }</template>\n`;
-
-  }
-
-  await writeFile(joinPath(distDir, `pages`, `${ page }/${ page }.html`), html, `utf8`);
-
-}
 
 export default async function buildPages() {
 
@@ -61,19 +32,12 @@ export default async function buildPages() {
   const criticalCSS   = await convertLESS(criticalLESS);
   hbs.registerPartial(`critical-css`, `${ criticalCSS }\n`);
 
-  // register HTML partials
+  // register Handlebars partials
   await registerPartials(hbs, srcDir);
 
   // build the app shell
   const appTemplatePath = joinPath(srcDir, `index.hbs`);
   const appHTML         = await buildTemplate(hbs, appTemplatePath);
   await writeFile(joinPath(distDir, `index.html`), appHTML, `utf8`);
-
-  // build the HTML for individual pages
-  const pages = await readDir(pagesDir);
-
-  for (const page of pages) {
-    await buildPageContent(page);
-  }
 
 }
