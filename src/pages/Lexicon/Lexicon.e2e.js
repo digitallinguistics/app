a51ba9076aee8a789fc05b603cd0b85a9e909004
Daniel W. Hieber <dwhieb@gmail.com>
2021-10-17 14:55:04 -0600

    get Lexicon page E2E test passing

--- src/pages/Lexicon/Lexicon.e2e.js
+++ src/pages/Lexicon/Lexicon.e2e.js
@@ -2,6 +2,7 @@ describe(`Lexicon`, function() {
 
   it(`renders the page`, function() {
     cy.visit(`/`);
+    cy.get(`#nav[data-loaded=true]`);
     cy.contains(`#nav li`, `Lexicon`)
     .click();
     cy.get(`#main[data-page=Lexicon]`);
