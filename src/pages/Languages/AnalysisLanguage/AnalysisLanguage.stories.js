eca62c34be770615a1be6abd66a39c26836fe95f
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-10-19 14:06:45 -0500

    create analysis language story

---
+++ src/pages/Languages/AnalysisLanguage/AnalysisLanguage.stories.js
@@ -0,0 +1,23 @@
+import html2element from '../../../utilities/html2element.js';
+import Language     from './AnalysisLanguage.js';
+
+export default {
+  title: `Languages/Analysis Language`,
+};
+
+export const AnalysisLanguage = () => {
+
+  const ul = html2element(`<ul class=list></ul>`);
+
+  const langView = new Language({
+    abbr: `eng`,
+    lang: `English`,
+    tag:  `en`,
+  }, 1);
+
+  const el = langView.render();
+
+  ul.appendChild(el);
+  return ul;
+
+};
