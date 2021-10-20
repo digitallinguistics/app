3d10c3833c747e49ec077d1d6fc8629c31c69c33
Daniel W. Hieber <dwhieb@gmail.com>
2021-10-20 01:29:45 -0600

    improve performance of Lexicon Worker

--- src/pages/Lexicon/LexiconWorker.js
+++ src/pages/Lexicon/LexiconWorker.js
@@ -1,45 +1,6 @@
 const storeName = `lexemes`;
 let db;
 
-function comparator(a, b) {
-  return a.displayName.localeCompare(b.displayName, undefined, { sensitivity: `base` });
-}
-
-function getLexemes(languageCID) {
-  return new Promise((resolve, reject) => {
-
-    const txn     = db.transaction(storeName);
-    const results = [];
-
-    txn.onabort    = () => reject(txn.error);
-    txn.oncomplete = () => resolve(results);
-    txn.onerror    = () => reject(txn.error);
-
-    const store = txn.objectStore(storeName);
-    const req   = store.index(`displayName`).openCursor();
-
-    req.onsuccess = ev => {
-
-      const cursor = ev.target.result;
-
-      if (!cursor) return;
-
-      const lexeme = cursor.value;
-
-      if (
-        lexeme.language === languageCID
-        && !lexeme.deleted
-      ) {
-        results.push(lexeme);
-      }
-
-      cursor.continue();
-
-    };
-
-  });
-}
-
 function getRandomString() {
   return (Math.random() + 1).toString(36).substring(7);
 }
@@ -63,7 +24,7 @@ function initializeDatabase() {
  * Utility function for testing the database. Keep this in the code for the time being.
  * @returns {Promise}
  */
-function populateDatabase() {
+function populateDatabase() { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
 
     const lexemes = [];
@@ -91,20 +52,37 @@ function populateDatabase() {
   });
 }
 
-async function renderList(operationID, { language }) { // eslint-disable-line no-unused-vars
+function renderList(operationID, { language }) { // eslint-disable-line no-unused-vars
 
-  // await populateDatabase();
-  const lexemes = await getLexemes(language);
+  const txn = db.transaction(storeName);
 
-  // lexemes.sort(comparator);
+  txn.onabort = console.error;
+  txn.onerror = console.error;
 
-  const last = lexemes.pop();
+  const store = txn.objectStore(storeName);
+  const req   = store.index(`displayName`).openCursor();
 
-  for (const lexeme of lexemes) {
-    self.postMessage({ data: lexeme, done: false, operationID });
-  }
+  req.onsuccess = ev => {
 
-  self.postMessage({ data: last, done: true, operationID });
+    const cursor = ev.target.result;
+
+    if (!cursor) {
+      self.postMessage({ done: true, operationID });
+      return;
+    }
+
+    const lexeme = cursor.value;
+
+    if (
+      lexeme.language === language
+      && !lexeme.deleted
+    ) {
+      self.postMessage({ data: lexeme, done: false, operationID });
+    }
+
+    cursor.continue();
+
+  };
 
 }
 
