4f185de3df5070712ba979d8220992352d491160
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-11-04 13:53:52 -0500

    implement MLSE name field
    *incorrect saving behavior*

--- src/pages/Languages/Orthography/Orthography.js
+++ src/pages/Languages/Orthography/Orthography.js
@@ -1,7 +1,9 @@
-import NotesList from '../../../components/NotesList/NotesList.js';
-import styles    from './Orthography.less';
-import template  from './Orthography.hbs';
-import View      from '../../../core/View.js';
+import debounce              from '../../../utilities/debounce.js';
+import MultiLangStringEditor from '../../../components/MultiLangStringEditor/MultiLangStringEditor.js';
+import NotesList             from '../../../components/NotesList/NotesList.js';
+import styles                from './Orthography.less';
+import template              from './Orthography.hbs';
+import View                  from '../../../core/View.js';
 
 export default class Orthography extends View {
 
@@ -21,9 +23,10 @@ export default class Orthography extends View {
     this.el.addEventListener(`click`, ({ target }) => {
 
       if (target.classList.contains(`js-orthography__cancel-button`)) {
-        this.nameInput.value = this.orthography.name;
+        this.nameInput.value = this.orthography.name.default;
+
         this.abbrInput.value = this.orthography.abbreviation;
-        this.updatePreview(this.orthography.name, this.orthography.abbreviation);
+        this.updatePreview(this.orthography.name.default, this.orthography.abbreviation);
         return this.hideEditor();
       }
 
@@ -33,13 +36,13 @@ export default class Orthography extends View {
 
       if (target.classList.contains(`js-orthography__save-button`)) {
 
-        if (this.nameInput.checkValidity()) {
+        if (this.abbrInput.checkValidity()) {
           this.save();
-          this.updatePreview(this.orthography.name, this.orthography.abbreviation);
+          this.updatePreview(this.orthography.name.default, this.orthography.abbreviation);
           return this.hideEditor();
         }
 
-        this.nameInput.reportValidity();
+        this.abbrInput.reportValidity();
 
       }
 
@@ -61,23 +64,43 @@ export default class Orthography extends View {
     this.el.dataset.id = this.index;
     this.editButton    = this.el.querySelector(`.js-orthography__edit-button`);
     this.editor        = this.el.querySelector(`.js-orthography__editor`);
-    this.nameInput     = this.el.querySelector(`.js-orthography__name-input`);
+
+    this.renderOrthoName();
+    this.nameInput     = this.el.querySelector(`[name^='${ this.nameID }']`);
+
     this.abbrInput     = this.el.querySelector(`.js-orthography__abbr-input`);
 
-    this.nameInput.id    = this.nameID;
-    this.nameInput.value = this.orthography.name;
     this.abbrInput.id    = this.abbrID;
     this.abbrInput.value = this.orthography.abbreviation;
 
     this.el.querySelector(`.js-orthography__name-legend`).setAttribute(`for`, this.nameID);
     this.el.querySelector(`.js-orthography__abbr-legend`).setAttribute(`for`, this.abbrID);
 
-    this.updatePreview(this.orthography.name, this.orthography.abbreviation);
+    this.updatePreview(this.orthography.name.default, this.orthography.abbreviation);
     this.addEventListeners();
+
     this.renderNotes();
 
     return this.el;
 
+  }
+
+  renderOrthoName() {
+
+    const nameField = this.el.querySelector(`.js-orthography__name`);
+
+    const mlsEditor = new MultiLangStringEditor(this.orthography.name, {
+      fieldName:       `${ this.nameID }`,
+      id:              `${ this.nameID }`,
+      inputAttributes: { autocapitalize: `words`, placeholder: `e.g. Cyrillic` },
+
+    });
+
+    const helpText = this.el.querySelector(`.orthography__help-text`);
+
+    nameField.insertBefore(mlsEditor.render(), helpText);
+
+    nameField.addEventListener(`input`, debounce(this.updateName.bind(this), this.delay));
   }
 
   renderNotes() {
@@ -90,18 +113,28 @@ export default class Orthography extends View {
   }
 
   save() {
-    this.orthography.name         = this.nameInput.value;
     this.orthography.abbreviation = this.abbrInput.value;
   }
 
   showEditor() {
     this.el.classList.add(`editing`);
     this.nameInput.focus();
+    this.nameInput.select();
+  }
+
+  updateName(ev) {
+    const { name, value } = ev.target;
+    const regex = new RegExp(`orthography-name-${ this.index }-(?<abbr>.+)$`, `u`);
+    const abbr = regex.exec(name)?.groups?.abbr;
+    this.orthography.name.set(abbr, value);
+    return this.save();
   }
 
   updatePreview(name, abbreviation) {
+    name ||= `[none]`;
     this.el.querySelector(`.js-orthography__preview`)
-    .innerHTML = `<span class=txn>${ name }</span> (${ abbreviation })`;
+    .innerHTML = `<span><span class=label> Name </span><span class=txn>${ name }</span></span><span><span class=label>Abbreviation
+    </span><span class=orthography__mono>${ abbreviation }</span></span>`;
   }
 
 }
