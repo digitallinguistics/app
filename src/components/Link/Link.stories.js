34cd9f622322cf5645ac532b9742546aa9cdb641
vadekh <36901345+vadekh@users.noreply.github.com>
2021-11-08 17:15:06 -0600

    FIX: external link styles (#297)
    closes #297

--- src/components/Link/Link.stories.js
+++ src/components/Link/Link.stories.js
@@ -5,5 +5,5 @@ export default {
 };
 
 export const SamePage   = () => `<p>This is a paragraph that <a class=link href=#>contains a same-page link</a>.</p>`;
-export const SameOrigin = () => `<p>This is a paragraph that <a class=link href=https://digitallinguistics.io>contains a same-origin link</a>.</p>`;
+export const SameOrigin = () => `<p>This is a paragraph that <a class=link href=https://app.digitallinguistics.io>contains a same-origin link</a>.</p>`;
 export const External   = () => `<p>This is a paragraph that <a class=link href=https://twitter.com>contains an external link</a>.</p>`;
