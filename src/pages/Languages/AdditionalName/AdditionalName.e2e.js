93660d1ae45427eec03425ecedd410e34a1f5276
Vade Kamenitsa-Hale <kamenitsahal@wisc.edu>
2021-12-14 13:45:48 -0600

    finish additionalName note test

--- src/pages/Languages/AdditionalName/AdditionalName.e2e.js
+++ src/pages/Languages/AdditionalName/AdditionalName.e2e.js
@@ -24,18 +24,18 @@ describe(`AdditionalName`, function() {
       cy.get(`.js-additional-name__save-button`)
       .click();
 
+      // add a note
+      cy.get(`.js-notes-list__add-note-button`)
+      .click();
+
+      cy.get(`.js-note__text-input`)
+      .clear()
+      .type(`This is text in a note`);
+
+      cy.get(`.js-note__save-button`)
+      .click();
+
     });
-
-    // add a note
-    cy.get(`.js-notes-list__add-note-button`)
-    .click();
-
-    cy.get(`.js-note__text-input`)
-    .clear()
-    .type(`This is text in a note`);
-
-    cy.get(`.js-note__save-button`)
-    .click();
 
     // switch page and back to check that the changes were saved
 
