076cb3faa32118accc09498c37fad0ccdd8fa89f
vadekh <36901345+vadekh@users.noreply.github.com>
2021-12-17 13:59:02 -0600

    NEW: use monospace font for language code (#387)
    closes #387

---
+++ src/components/Code/Code.stories.js
@@ -0,0 +1,7 @@
+import '../../../dist/components/Code/Code.css';
+
+export default {
+  title: `Components/Code`,
+};
+
+export const Code = () => `<p>This is some text with a <span class=code>code</span> in it.</p>`;
