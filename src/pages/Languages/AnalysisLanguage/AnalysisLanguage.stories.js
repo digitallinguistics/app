d047bd05a760849e4782b3f80cbcd125a4ead1eb
Daniel W. Hieber <dwhieb@gmail.com>
2022-01-01 16:08:56 -0700

    Blank + Populated stories for Analysis Language

--- src/pages/Languages/AnalysisLanguage/AnalysisLanguage.stories.js
+++ src/pages/Languages/AnalysisLanguage/AnalysisLanguage.stories.js
@@ -5,21 +5,35 @@ export default {
   title: `Languages/Analysis Language`,
 };
 
-export const AnalysisLanguage = () => {
+const Template = ({ data }) => {
 
-  const ul = html2element(`<ul class=list></ul>`);
+  const ul       = html2element(`<ul class=list></ul>`);
+  const langView = new AnalysisLang(data, 1);
+  const el       = langView.render();
 
-  const langView = new AnalysisLang({
+  el.style.border = `var(--border)`;
+  ul.appendChild(el);
+
+  return ul;
+
+};
+
+export const Blank = Template.bind({});
+
+Blank.args = {
+  data: {
     abbreviation: ``,
     language:     ``,
     tag:          ``,
-  }, 1);
+  },
+};
 
-  const el = langView.render();
+export const Populated = Template.bind({});
 
-  el.style.border = `var(--border)`;
-
-  ul.appendChild(el);
-  return ul;
-
+Populated.args = {
+  data: {
+    abbreviation: `fra`,
+    language:     `French`,
+    tag:          `fra`,
+  },
 };
