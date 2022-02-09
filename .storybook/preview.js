4f16671108aa70ca0d2774a3264812ebb4ed250b
Daniel W. Hieber <dwhieb@gmail.com>
2022-02-08 21:38:28 -0700

    Box Input

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import './storybook.css';
 import '../dist/classes/Abbreviation/Abbreviation.css';
 import '../dist/classes/BlockHeader/BlockHeader.css';
+import '../dist/classes/BoxInput/BoxInput.css';
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
